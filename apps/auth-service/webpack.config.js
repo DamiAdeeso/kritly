4d783e72aa4294534a4b72b3b755456fa7b7e497
Oluwadamilola <oluwadamilola.adeeso@unionsystems.com>
2025-08-29 13:48:45 +0100

    chore:updates

--- apps/auth-service/webpack.config.js
+++ apps/auth-service/webpack.config.js
@@ -1,5 +1,8 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 
 module.exports = composePlugins(withNx(), (config) => {
+  // Configure for Node.js environment
+  config.target = 'node';
+  
   return config;
 });
